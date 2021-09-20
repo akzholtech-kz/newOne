3526a6864e058d45ad62c07e26c27898b19ea4b3
akzholtech-kz <akzhol.nuradin@nu.edu.kz>
2021-09-20 16:49:01 +0600

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HeaderComponent } from './header/header.component';
+import { MainDetailComponent } from './main/main-detail/main-detail.component';
+import { MainItemsComponent } from './main/main-list/main-items/main-items.component';
+import { MainListComponent } from './main/main-list/main-list.component';
+import { MainComponent } from './main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    MainDetailComponent,
+    MainListComponent,
+    MainItemsComponent,
+    EmployeeComponent,
+    EmployeeEditComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
